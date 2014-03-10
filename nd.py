9c2791eacdca5f9ac72cb7df54a5470107a80154
opkest <opkest@gmail.com>
2014-03-10 15:03:23 +0200

    Sukurtas failas

---
+++ nd.py
@@ -0,0 +1,4 @@
+'Apskaiciuoti nurodytoje direktorijoje esanciu failu statistika ir ja isvesti '
+'i faila. Statistika: pirmiausia isvesti bendrai visuose failuose kokie ir kie'
+'k kartu pasikartoja a) zodziai b)simboliai. Antra, i ta pati faila isvesti ta'
+' pacia statistika apskaiciuota kiekvienam failui atskirai.'
