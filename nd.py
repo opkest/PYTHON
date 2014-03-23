79724a42a6974d86289c3b99af11c01580f9e926
opkest <opkest@gmail.com>
2014-03-23 20:39:50 +0200

    Deleted task. Added path to directory.

--- nd.py
+++ nd.py
@@ -1,4 +1,10 @@
-'Apskaiciuoti nurodytoje direktorijoje esanciu failu statistika ir ja isvesti '
-'i faila. Statistika: pirmiausia isvesti bendrai visuose failuose kokie ir kie'
-'k kartu pasikartoja a) zodziai b)simboliai. Antra, i ta pati faila isvesti ta'
-' pacia statistika apskaiciuota kiekvienam failui atskirai.'
+#! /usr/bin/env python
+# -*- coding: utf-8  -*-
+import sys
+
+if len(sys.argv)<2:
+    print("Enter path to directory: python nd.py path_to_directory")
+    quit()
+else:
+    path_to_directory = sys.argv[1]
+
