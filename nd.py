0a89f3bc0f19931b65f951e54a3726ede6b19920
opkest <opkest@gmail.com>
2014-03-23 23:27:42 +0200

    Added function to calcuate symbols. Returns defaultdict of symbols.

--- nd.py
+++ nd.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8  -*-
 import sys
-
+import collections
+# Request to enter path to directory.
 if len(sys.argv)<2:
     print("Enter path to directory: python nd.py path_to_directory")
     quit()
 else:
     path_to_directory = sys.argv[1]
+
+
+# Function to calcuate symbols
+def calc_symbols(info)
+    symbols = collections.defaultdict(int)
+    for i in info:
+        symbols[i] += 1
+    return symbols
 
