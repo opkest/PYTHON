5349283e9724befd32c9d27468ce024931a60dac
opkest <opkest@gmail.com>
2014-04-03 10:07:34 +0300

    Changed request to enter not only path to directory, but also enter name of statistics file.

--- nd.py
+++ nd.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8  -*-
 import sys
-import os
+
 import collections
 import re
 # Request to enter path to directory.
-if len(sys.argv)<2:
-    print("Enter path to directory: python nd.py path_to_directory")
+if len(sys.argv) < 3:
+    print("Enter path to directory: python nd.py path_to_directory"
+          "statistics_file_name.txt")
     quit()
 else:
     path_to_directory = sys.argv[1]
