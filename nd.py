f9f03cec976e2959112bc3a32c43b9649c7bc4df
opkest <opkest@gmail.com>
2014-04-03 10:36:51 +0300

    Imported os library, added line to get list of files in specified directory. Comments added to docstrings.

--- nd.py
+++ nd.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8  -*-
 import sys
-
+import os
 import collections
 import re
-# Request to enter path to directory.
+
 if len(sys.argv) < 3:
+    '''Request to enter path to directory and statistics file name'''
     print("Enter path to directory: python nd.py path_to_directory"
           "statistics_file_name.txt")
     quit()
@@ -13,15 +14,18 @@ else:
     path_to_directory = sys.argv[1]
 
 
-# Function to calcuate symbols
 def calc_symbols(info):
+    '''Function to count symbols'''
     symbols = collections.defaultdict(int)
     for i in info:
         symbols[i] += 1
     return symbols
 
-# Function to calcuate words
+
 def calc_words(info):
+    '''Function to count words'''
     words = collections.Counter(map(str.lower, re.split('\W+', info)))
     return words
 
+
+file_list = os.listdir(path_to_directory)
